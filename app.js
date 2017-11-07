a1965ba6ff065b2d6a1becf493e4a37d47ab5612
pereztjacob <pereztjacob@gmail.com>
2017-11-06 17:58:45 -0800

    repair clickhandle event -- functional app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let globalI = 0;
+
 // array containing image file locations
 const picArray = ['images/bag.jpg', 'images/banana.jpg', 'images/bathroom.jpg', 'images/boots.jpg', 'images/breakfast.jpg', 
     'images/bubblegum.jpg', 'images/chair.jpg', 'images/cthulhu.jpg', 'images/dog-duck.jpg', 'images/dragon.jpg', 'images/pen.jpg',
@@ -7,7 +9,8 @@ const picArray = ['images/bag.jpg', 'images/banana.jpg', 'images/bathroom.jpg', 
     'images/usb.gif', 'images/water-can.jpg', 'images/wine-glass.jpg'];
 
 // array to be used in shuffle function    
-let myArray = ['1','2','3','4','5','6','7','8','9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '0'];
+let myArray = ['0','1','2','3','4','5','6','7','8','9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+        '0', '1', '2', '3'];
 
 // randomizes numbers in an array. by accessing the first three, i get three random ints with no duplicates.
 function shuffle(spread) {
@@ -31,22 +34,27 @@ function tester(source){
     img.setAttribute('class', 'pix');
     pics.appendChild(img);
 }
+
+// calls render function
 for(let i = 0; i < 3; i++){
     tester(picArray[myArray[i]]);
 }
 
-function event(){
-    let x = document.getElementsByClassName('pix')[0];
-    let y = document.getElementsByClassName('pix')[1];
-    let z = document.getElementsByClassName('pix')[2]; 
-    console.log(x, y, z);
-    x.addEventListener('click', clickHandler);
+// creates event listener
+let pict = document.getElementById('imageSpace');
+pict.addEventListener('click', clickHandler);
+if(globalI >= 20){
+    pict.removeEventListener('click', clickHandler);
 }
-event();
 
+// function to remove item, add new item
 function clickHandler(e){
+    globalI++;
+    console.log(globalI);
     const clickedItem = e.target;
-    clickedItem.remove;
+    if(clickedItem.id === 'imageSpace'){
+        return;
+    }
+    clickedItem.remove();
+    tester(picArray[myArray[globalI + 3]]);
 }
-
-
