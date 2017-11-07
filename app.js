3ec8889c144a543acd0d807965f5275321b0b230
pereztjacob <pereztjacob@gmail.com>
2017-11-07 08:41:34 -0800

    mondays lab

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const picArray = ['images/bag.jpg', 'images/banana.jpg', 'images/bathroom.jpg', 
 let myArray = ['0','1','2','3','4','5','6','7','8','9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
         '0', '1', '2', '3'];
 
+// constructor function containing picarray values, indexes, and click values
+
+
 // randomizes numbers in an array. by accessing the first three, i get three random ints with no duplicates.
 function shuffle(spread) {
     var j, x, i;
@@ -53,6 +56,7 @@ function clickHandler(e){
     console.log(globalI);
     const clickedItem = e.target;
     if(clickedItem.id === 'imageSpace'){
+        globalI--;
         return;
     }
     clickedItem.remove();
